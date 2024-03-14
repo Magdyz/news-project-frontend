281bc28e90ec175b71eafaef4c502455ff4694a0
Magz <magdy_176@hotmail.com>
2024-03-14 11:43:03 +0000

    added voting button on articles

--- components/VoteArticle.jsx
+++ components/VoteArticle.jsx
@@ -7,16 +7,16 @@ const VoteArticle = ({ article_id, article }) => {
   const [error, setError] = useState(null);
 
   const addVote = (article_id) => {
+    setVote(vote + 1);
     axios
       .patch(
         `https://project-nc-news-xu65.onrender.com/api/articles/${article_id}`,
         { inc_votes: 1 }
       )
-      .then(({ data }) => {
-        setVote((prevVote) => prevVote + 1);
-      })
+      .then(({ data }) => {})
       .catch((err) => {
-        setVote((prevVote) => prevVote - 1);
+        setVote(vote - 1);
+
         return Promise.reject(new Error(err));
       });
   };
